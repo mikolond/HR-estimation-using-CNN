d1df9cf0fff31c89406973338da5db53e33b480b
Ondřej Mikoláš <mikolond@fel.cvut.cz>
2024-09-25 16:30:55 +0200

    Model modified for more images, started making loss for extractor

--- loss.py
+++ loss.py
@@ -1,10 +1,24 @@
 import torch.nn as nn
-
+import torch
+import numpy as np
 
 class ExtractorLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, n_s, n_f):
         super(ExtractorLoss, self).__init__()
+        self.n = n_s
         
+    
+    def PSD(self, x, y):
+        '''
+        Returns power spectra density of given sequence inside list x
+        params: x :sequence of numbers
+                y :the ground true frequency
+        return: 
+        '''
+        term1 = torch.zeros(self.n, 1)
+        term2 = torch.zeros(self.n, 1)
+        for i in range(n):
+            term1[i] = torch.x[i]*np.cos(2 * np.pi* y)
 
     def forward(self, x, y):
         return self.loss(x, y)
--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 import torch
+import numpy as np
 
-print("cuda available:", torch.cuda.is_available())
+a = np.array([[1,2],[2,3]])
+x = np.array([3,4]).T
+a = torch.Tensor(a)
+x = torch.Tensor(x)
+
+y = torch.matmul(a,x)
+
+print("y:",y)
