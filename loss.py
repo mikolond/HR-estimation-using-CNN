5e2c2b0800ca47301e573f609250408d81252f94
Ondřej Mikoláš <mikolond@fel.cvut.cz>
2024-09-29 14:48:30 +0200

    extractor loss finished

--- loss.py
+++ loss.py
@@ -3,22 +3,46 @@ import torch
 import numpy as np
 
 class ExtractorLoss(nn.Module):
-    def __init__(self, n_s, n_f):
+    def __init__(self):
         super(ExtractorLoss, self).__init__()
-        self.n = n_s
         
     
-    def PSD(self, x, y):
+    def PSD(self, x, f_true, fs):
         '''
         Returns power spectra density of given sequence inside list x
-        params: x :sequence of numbers
-                y :the ground true frequency
+        params: x :list of numbers
+                f_true : ground true frequency
+                fs : sampling frequency
         return: 
         '''
-        term1 = torch.zeros(self.n, 1)
-        term2 = torch.zeros(self.n, 1)
-        for i in range(n):
-            term1[i] = torch.x[i]*np.cos(2 * np.pi* y)
+        term1 = 0
+        term2 = 0
+        for i in range(len(x)):
+            term1 += x[i] * np.cos(2 * np.pi * f_true * i / fs)
+            term2 += x[i] * np.sin(2 * np.pi * f_true * i / fs)
+        return term1**2 + term2**2
+    
+    def SNR(self, x, f_true, fs, delta, f_range):
+        '''
+        Returns signal to noise ratio of given sequence inside list x
+        params: x :list of numbers
+                f_true : ground true frequency
+                delta : offset from the true frequency
+                f_range : list(f_min, f_max) all possible frequencies
+        return: 
+        '''
+        f_min, f_max = f_range
+        f_wanted =list(range(f_true - delta, f_true + delta + 1))
+        f_unwanted = list(range(f_min, f_true - delta)) + list(range(f_true + delta + 1, f_max+1))
+        term1 = 0
+        term2 = 0
+        for f in f_wanted:
+            term1 += self.PSD(x, f, fs)
+        for f in f_unwanted:
+            term2 += self.PSD(x, f, fs)
 
-    def forward(self, x, y):
-        return self.loss(x, y)
+        return 10 * torch.log10(term1 / term2)
+
+
+    def forward(self, x, f_true, fs, delta, f_range):
+        return self.SNR(x, f_true, fs, delta, f_range)
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-import torch
 import numpy as np
 
-a = np.array([[1,2],[2,3]])
-x = np.array([3,4]).T
-a = torch.Tensor(a)
-x = torch.Tensor(x)
 
-y = torch.matmul(a,x)
+x1 = 10
+x2 = 100
+y = 5
+f = 35
 
-print("y:",y)
+f_min, f_max = 10,100
+f_true = 35
+delta = 5
+f_wanted =list(range(f_true - delta, f_true + delta + 1))
+f_unwanted = list(range(f_min, f_true - delta)) + list(range(f_true + delta + 1, f_max+1))
+
+print("wanted",f_wanted)
+print("unwanted",f_unwanted)
